63837300fde2018a0c66d8189b5430a84eee324a
Shivam Deep <136151392+shivampeed@users.noreply.github.com>
2026-04-16 17:38:31 +0530

    Update Eventloop3.js

--- Chapter 09 - libuv & event loop/Code/Eventloop3.js
+++ Chapter 09 - libuv & event loop/Code/Eventloop3.js
@@ -22,3 +22,14 @@ process.nextTick(() => console.log("1st Process.nexttick"))
 
 console.log("last line of program")
 
+// last line of program
+// 1st Process.nexttick
+// promise
+// 1st timer
+// 1st setImmediate
+// Hii I am Nodejs Developer.
+
+// 2nd Process.nexttick
+// 2nd setImmediate
+// 2nd timer
+
